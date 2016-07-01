0bae52b7dbcdf56b9efa4f81cd2b1a6e93619847
Andrea Barisani <andrea@inversepath.com>
2016-07-01 10:44:42 +0200

    add license headers

--- scc2_aes_test.c
+++ scc2_aes_test.c
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2016 Inverse Path S.r.l.
+ *
+ * https://github.com/inversepath/mxc-scc2
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation under version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ */
+
 static int aes_encrypt_test(void)
 {
 	int part_no, length;
